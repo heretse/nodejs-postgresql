935d8279a3a3334c69126b2ddba0bd436cd34236
Winston Hsieh <heretse@gmail.com>
2022-08-23 23:53:42 +0800

    Run with local postgres database

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 const express = require('express');
-const { Client } = require('pg');
-const connectionString = 'postgres://postgres:postgres@localhost:5432/database';
+const { Client, Pool } = require('pg');
+// const connectionString = 'postgres://postgres:postgres@localhost:5432/northwind';
+// const client = new Client({
+//     connectionString: connectionString
+// })
 
-const client = new Client({
-    connectionString: connectionString
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'northwind',
+    max: 50,
+    idleTimeoutMillis: 10000,
+    connectionTimeoutMillis: 3000,
 });
 
-client.connect();
+// client.connect()
 
-var app = express();
+var app = express()
 
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000)
 
-app.get('/', function (req, res, next) {
-    client.query('SELECT * FROM Employee where id = $1', [1], function (err, result) {      
+app.get('/:id', async function (req, res, next) {
+    const id = req.params.id
+
+    client = await pool.connect()
+    client.query('SELECT * FROM Products where product_id = $1', [id], function (err, result) {
+        client.release()
         if (err) {
-            console.log(err);
-            res.status(400).send(err);
+            console.error('Error executing query', err.stack)
+            res.status(400).send(err)
         }
-        res.status(200).send(result.rows); 
-    });
-});
+        console.log(result.rows)
+        res.status(200).send(result.rows)
+    })
+})
 
-app.listen(4000, function () {
-    console.log('Server is running.. on Port 4000');
-});
+app.listen(3000, function () {
+    console.log('Server is running.. on Port 3000');
+})
